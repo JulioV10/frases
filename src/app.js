5de9cd8ce1cf04eb7679fe03000c82bf0141b75f
JulioV10 <julio.vargas11@hotmail.com>
2023-01-24 23:53:35 +0000

    frases

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    // console.log(pronoun[i]);
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < ext.length; m++) {
+          let frases = pronoun[i] + "." + adj[j] + "." + noun[k] + ext[m];
+          console.log(frases);
+        }
+        // document.getElementById("phrase").innerHTML = frases;
+      }
+    }
+  }
 };
